aba006cbc2323e130a9786142aa1a2e2e8b0ad13
Francis Leo <drypasta34@gmail.com>
2025-07-22 02:41:36 +0800

    Restart
    Removed all codes,scenes and aseetes

--- Scripts/resources_ui.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var virtue_label = $VirtuePoints
-var virtue = Global.virtue_points
-
-func _process(_delta: float) -> void:
-	virtue_label.text = "Virtue Points: " + str(virtue)
--- Scripts/upgrade_menu.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("upgrade_menu"):
-		visible = not visible
-
-func _on_button_pressed() -> void:
-	visible = false
